93667be64db637529e655403168e41a9715cdb37
Zdenko Sadojevic <zdenko.sadojevic@sinch.com>
2023-09-20 11:34:50 +0300

    Added global var

--- recursion.py
+++ recursion.py
@@ -1,12 +1,19 @@
-#Test
+#global g
+g = 10
 
 def f(n):
+  global g  # if not declared as global, local variable g will be created
   print("f start " + str(n))
+  g += n
+  print("f start g=" + str(g))
   if n == 0:
       return
   f(n-1)
   print("f end " + str(n))
- 
+  print("f end g=" + str(g))
+  
+g += 1
 print("start")
 f(3)
+print("end g=" + str(g))
 print("end")
